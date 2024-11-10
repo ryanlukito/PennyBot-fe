cc5466480d51b65725028e27bd49350f17281782
ryanlukito <ryanlukito13@gmail.com>
2024-11-10 21:16:55 +0700

    adding login feature using jwt

---
+++ app/InputExpenses/layout.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import axios, { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+interface UserResponse {
+  user: string | null;
+  error: AxiosError | null;
+}
+
+export default function PageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    (async () => {
+      const { error } = await getUser();
+      if (error) {
+        router.push("/");
+        return;
+      }
+      setIsSuccess(true);
+    })();
+  }, [router]);
+
+  if (!isSuccess) {
+    return <h1>Loading...</h1>;
+  }
+  return <main>{children}</main>;
+}
+
+async function getUser(): Promise<UserResponse> {
+  try {
+    const { data } = await axios.get("/api/auth/me");
+    return {
+      user: data,
+      error: null,
+    };
+  } catch (e) {
+    const error = e as AxiosError;
+    return {
+      user: null,
+      error,
+    };
+  }
+}
---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,44 @@
+import { COOKIE_NAME } from "@/constants";
+import { error } from "console";
+import { verify } from "jsonwebtoken";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get(COOKIE_NAME);
+  if (!token) {
+    return NextResponse.json(
+      {
+        message: "Unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const secret = process.env.JWT_SECRET || "";
+
+  try {
+    verify(token.value, secret);
+
+    const response = {
+      message: "Super Top Secret User!",
+    };
+
+    return new Response(JSON.stringify(response), {
+      status: 200,
+    });
+  } catch (e) {
+    return NextResponse.json(
+      {
+        message: "Something went wrong",
+        e,
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+}
