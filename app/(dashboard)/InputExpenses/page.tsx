031e71658e037fbde288f3ed103b86e162a2e8c9
alexchtst-01 <acinatra@gmail.com>
2024-11-25 19:19:43 +0700

    try image upload pt2

--- app/(dashboard)/InputExpenses/page.tsx
+++ app/(dashboard)/InputExpenses/page.tsx
@@ -143,27 +143,26 @@ const InputExpensesPage = () => {
                 required
               />
             </div>
+            <div className="w-[26.302vw] h-[32.76vw] bg-[#E2ECEA] text-[1.042vw] mb-[4.1vw] flex items-center justify-center rounded-[0.521vw]">
+              <div className="flex flex-col items-center justify-center">
+                <input
+                  type="file"
+                  id="invoiceFile"
+                  name="invoiceFile"
+                  className="bg-[#E2ECEA] w-[31.771vw] h-[2.396vw] rounded-[0.521vw] px-[1vw]"
+                />
+                <label htmlFor="invoiceFile" className="cursor-pointer">
+                  <FaPlus className="text-[4vw]" />
+                  <h1 className="text-[1vw]">Upload an Invoice</h1>
+                </label>
+              </div>
+            </div>
             <button className="w-[8.177vw] h-[2.865vw] font-bold text-[1.042vw] rounded-[0.521vw] bg-[#22B786] text-white hover:scale-[102%] ease-in-out duration-300 ml-[7.7vw]">
               Save
             </button>
           </div>
-          <div className="w-[26.302vw] h-[32.76vw] bg-[#E2ECEA] text-[1.042vw] mb-[4.1vw] flex items-center justify-center rounded-[0.521vw]">
-        <div className="flex flex-col items-center justify-center">
-          <input
-            type="file"
-            id="invoiceFile"
-            name="invoiceFile"
-            className="bg-[#E2ECEA] w-[31.771vw] h-[2.396vw] rounded-[0.521vw] px-[1vw]"
-          />
-          <label htmlFor="invoiceFile" className="cursor-pointer">
-            <FaPlus className="text-[4vw]" />
-            <h1 className="text-[1vw]">Upload an Invoice</h1>
-          </label>
-        </div>
-      </div>
         </form>
       </div>
-      
     </section>
   );
 };
